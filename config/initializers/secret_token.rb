00736bb572b050ad61b2a84531d0526c91523b64
Niko Kiuru <niko@kiuru.net>
2013-09-25 11:39:46 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LyricalBattle::Application.config.secret_key_base = '7731c9ba38c4405ea99c67e74262c20d77de5e9cf0e0e6a91b2ab0f321fd20ab4c3ef7114bdf041dd69a4a605f9c89b1fee6d40e17b4d8b22f6fca8aa5aa0443'
