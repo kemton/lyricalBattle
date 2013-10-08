644d9a2e336010799b859fe1c4e680b809b62434
Niko Kiuru <niko@kiuru.net>
2013-10-08 22:25:15 +0300

    different channel could publish their own messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,15 +4,14 @@ class MessagesController < ApplicationController
     @messages = Message.all
   end
 
-  def new
-    @message = Message.new
-  end
+  #def new
+  #  @message = Message.new
+  #end
 
   def create
     @message = Message.create!(params[:message].permit(:content, :sender, :room_number))
-    #PrivatePub.publish_to("/messages/new", message: @message)
-    #PrivatePub.publish_to("/messages/new", "alert('#{@message.content}');")
-    # redirect to show action: redirect_to message_path(@message)
+    @channel = params[:message][:room_number]
+    PrivatePub.publish_to("/channels/#{@channel}", message: @message)
     #redirect_to messages_path
   end
 
