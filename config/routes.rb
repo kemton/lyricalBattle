f2d78a2933e51fe386c52661d52af01a5c7296f6
Niko Kiuru <niko@kiuru.net>
2013-10-25 14:15:43 +0300

    remove rooms resources

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,6 @@ LyricalBattle::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :messages
-  resources :rooms
   resources :battles
 
   # Example resource route with options:
