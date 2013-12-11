737e0157add7876c4af1cc95bc520dc2351501b4
Niko Kiuru <niko@kiuru.net>
2013-12-11 22:30:08 +0200

    delete facebook debug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,6 @@
 class SessionsController < ApplicationController
 	def create
-		#user = User.from_omniauth(env["omniauth.auth"])
-
-		#debug
-		user = User.new
-		user.provider = "facebook"
-		user.uid = "123456"
-		user.name = "Niko Kiuru"
-		user.oauth_token = "ABCDE"
-		user.oauth_expires_at = Time.at(1321747205)
-		user.save!
-		
+		user = User.from_omniauth(env["omniauth.auth"])
 		session[:user_id] = user.id
 		redirect_to root_url
 	end
